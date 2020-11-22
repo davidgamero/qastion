7d828807d274df1a18fb654ebb31adc569b4b6a4
David Gamero <david340804@gmail.com>
2020-11-22 11:30:28 -0500

    suggestion box

--- src/components/MessageInput/index.js
+++ src/components/MessageInput/index.js
@@ -8,7 +8,9 @@ import { useState } from 'react';
 const InputForm = styled.form`
 width: 100%;
 display: flex;
-justify-content: center;
+flex-direction: column;
+align-items: center;
+align-content: center;
 `
 
 const TextInput = styled.input`
@@ -27,7 +29,34 @@ background-color: #5c585c;
 ::placeholder {
   color: lightgrey;
 }
+display: flex;
+flex-direction: column;
 `;
+
+const SuggestionBox = styled.div`
+width: 70%;
+background-color: #dafebb;
+padding: 0px 10px;
+border-radius: 0.2rem;
+border: none;
+display: flex;
+flex-direction: column;
+`;
+
+const QuastionTitle = styled.p`
+color: #1b3c13;
+font-size: 0.8rem;
+margin: 10px 0;
+margin-bottom: 5px;
+`
+
+const SuggestionText = styled.p`
+color: #244f19;
+font-size: 1rem;
+margin: 10px 0;
+margin-top: 0;
+`
+
 
 function MessageInput({ onChange, onSend }) {
   const [messageText, setMessageText] = useState('');
@@ -48,6 +77,14 @@ function MessageInput({ onChange, onSend }) {
 
   return (
     <InputForm onSubmit={handleSubmit}>
+      <SuggestionBox>
+        <QuastionTitle>
+          {'Qastion Suggestion'}
+        </QuastionTitle>
+        <SuggestionText>
+          {'Similar to: "When is our new deadline?"'}
+        </SuggestionText>
+      </SuggestionBox>
       <TextInput placeholder="Type a new message" onChange={handleChange} />
 
     </InputForm>
